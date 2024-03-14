f6d1854728e408e8f89de72b3c403b1a9afad263
bagusyatma <42886214+bagusyatma@users.noreply.github.com>
2024-03-14 22:46:05 +0700

    add feature health checker

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -59,6 +59,14 @@ const createServer = async (container) => {
     },
   ]);
 
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => ({
+      value: 'forum-api ✅',
+    }),
+  });
+
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
